c2cfdb25ce44aea6da497a646748a8017a1d192b
goder-0 <goder0@naver.com>
2023-11-15 17:20:14 +0900

    feat: ChristmasService 구현 - 날짜 등록 - 주문 등록 - 혜택 계산 - 배지 결정 - 증정 결정 - 예상 금액 결정

---
+++ src/main/java/christmas/service/ChristmasService.java
@@ -0,0 +1,87 @@
+package christmas.service;
+
+import christmas.model.badge.Badge;
+import christmas.model.benefit.Benefit;
+import christmas.model.benefit.ChristmasDiscount;
+import christmas.model.benefit.Present;
+import christmas.model.benefit.SpecialDiscount;
+import christmas.model.benefit.WeekdayDiscount;
+import christmas.model.benefit.WeekendDiscount;
+import christmas.model.menu.Menu;
+import christmas.model.order.Order;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ChristmasService {
+    private List<Benefit> benefits = new ArrayList<>();
+    private List<Order> orders;
+    private LocalDate date;
+    private long totalPrice = 0;
+    private long totalBenefitPrice = 0;
+    private Order present = new Order(Menu.Empty, 1);
+
+    public ChristmasService() {
+        orders = new ArrayList<>();
+        benefits.addAll(List.of(
+                new WeekdayDiscount(),
+                new Present(),
+                new SpecialDiscount(),
+                new WeekendDiscount(),
+                new ChristmasDiscount()
+        ));
+    }
+
+    public void selectPurchaseDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void orderMenus(List<Order> orders) {
+        this.orders.addAll(orders);
+        totalPrice = 0L;
+        for (Order order : this.orders) {
+            totalPrice += order.getPrice();
+        }
+    }
+
+    private boolean isPresentable(Benefit benefit) {
+        return benefit instanceof Present;
+    }
+
+    private void removeBenefits() {
+        benefits.removeIf(benefit -> !benefit.isAble(date, orders));
+    }
+
+    public Long calculateBenefit() {
+        removeBenefits();
+        totalBenefitPrice = 0L;
+        for (Benefit benefit : benefits) {
+            benefit.update(date, orders);
+            totalBenefitPrice += benefit.getAmount();
+        }
+        return -totalBenefitPrice;
+    }
+
+    public Long calculateExpectedPrice() {
+        calculateBenefit();
+        return totalPrice + present.getPrice() - totalBenefitPrice;
+    }
+
+    public Badge checkBadge() {
+        return Badge.findByPrice(totalPrice);
+    }
+
+    public Order checkPresent() {
+        removeBenefits();
+        Optional<Benefit> champagnePresent = benefits.stream()
+                .filter(this::isPresentable)
+                .findAny();
+        champagnePresent.ifPresent(benefit -> present = ((Present) benefit).getOrder());
+        return present;
+    }
+
+    public Long getTotalPrice() {
+        return totalPrice;
+    }
+}
---
+++ src/test/java/christmas/service/ChristmasServiceTest.java
@@ -0,0 +1,74 @@
+package christmas.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.model.badge.Badge;
+import christmas.model.menu.Menu;
+import christmas.model.order.Order;
+import java.time.LocalDate;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChristmasServiceTest {
+
+    private ChristmasService christmasService;
+    private LocalDate date;
+    private List<Order> orders;
+
+    @BeforeEach
+    void beforeEach() {
+        christmasService = new ChristmasService();
+        date = LocalDate.of(2023, 12, 25);
+        orders = List.of(
+                new Order(Menu.BarbecueRib, 2),
+                new Order(Menu.RedWine, 1)
+        );
+    }
+
+    @Test
+    @DisplayName("메뉴를 주문하면 할인 전 총주문 금액이 결정된다.")
+    void orderMenus() {
+        christmasService.orderMenus(orders);
+        Assertions.assertThat(christmasService.getTotalPrice()).isEqualTo(168000L);
+    }
+
+    @Test
+    @DisplayName("혜택을 계산하면 혜택 금액이 결정된다."
+            + "해택내역 : 크리스마스:3400, 특별할인:1000, 증정할인:25000 ")
+    void calculateBenefit() {
+        christmasService.selectPurchaseDate(date);
+        christmasService.orderMenus(orders);
+        Long benefit = christmasService.calculateBenefit();
+        Assertions.assertThat(benefit).isEqualTo(-29400);
+    }
+
+    @Test
+    @DisplayName("예상 금액을 계산한다.")
+    void calculateExpectedPrice() {
+        christmasService.selectPurchaseDate(date);
+        christmasService.orderMenus(orders);
+        Assertions.assertThat(christmasService.calculateExpectedPrice()).isEqualTo(138600);
+    }
+
+    @Test
+    @DisplayName("증정 여부를 확인한다.")
+    void checkPresent() {
+        christmasService.selectPurchaseDate(date);
+        christmasService.orderMenus(orders);
+        Order order = christmasService.checkPresent();
+        Assertions.assertThat(order.getMenu()).isEqualTo(Menu.Champagne);
+    }
+
+    @Test
+    @DisplayName("배지 여부를 확인한다.")
+    void checkBadge() {
+        christmasService.selectPurchaseDate(date);
+        christmasService.orderMenus(orders);
+        Badge badge = christmasService.checkBadge();
+        Assertions.assertThat(badge).isEqualTo(Badge.SANTA);
+    }
+
+}
